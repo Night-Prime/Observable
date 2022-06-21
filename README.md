bf60f350477d4e28b5d2e69bdcfceb428ebd6b98
Night-Prime <danielabatibabatunde1@gmail.com>
2022-06-21 15:13:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-subjects-example-todo-app-du84wc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-subjects-example-todo-app-du84wc)
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { TodoService } from "./todo.service";
+import { TodoAddComponent } from "./todo-add.component";
+import { TodoListComponent } from "./todo-list.component";
+
+@NgModule({
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    TodoListComponent,
+    TodoAddComponent
+  ],
+  bootstrap: [AppComponent],
+  providers: [TodoService]
+})
+export class AppModule {}
